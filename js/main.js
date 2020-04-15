59de54bac304eab44499a2e185d410a9dbb27a65
tariktabakovic <tariktab97@gmail.com>
2020-04-15 15:31:50 -0400

    creates function for changing background & greeting, according to the time of day

--- js/main.js
+++ js/main.js
@@ -28,5 +28,27 @@ function addZero(n){
     return(parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
+// Set Background and Greeting
+function setBackgroundGreeting(){
+    let today = new Date(),
+    hour = today.getHours();
+
+    if(hour < 12){
+        // Morning Time
+        document.body.style.backgroundImage = "url('./img/morning.jpeg')";
+        greeting.textContent= 'Good Morning';
+    } else if(hour < 18){
+        // Afternoon Time
+        document.body.style.backgroundImage = "url('./img/afternoon.jpg')";
+        greeting.textContent= 'Good Afternoon';
+    } else {
+        // Evening Time
+        document.body.style.backgroundImage = "url('./img/night.jpg')";
+        greeting.textContent= 'Good Evening';
+        document.body.style.color = 'white';
+    }
+}
+
 // Run
 showTime();
+setBackgroundGreeting();
