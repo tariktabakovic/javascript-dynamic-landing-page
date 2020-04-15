1bed6af78db0d4681b472125b72d057f8e31997b
tariktabakovic <tariktab97@gmail.com>
2020-04-15 15:22:22 -0400

    creates function for adding a zero to the time when needed

--- js/main.js
+++ js/main.js
@@ -18,9 +18,14 @@ function showTime(){
     hour = hour % 12 || 12;
 
     // Output Time
-    time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`
 
     setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function addZero(n){
+    return(parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
 // Run
